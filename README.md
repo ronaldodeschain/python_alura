45f04c753b71f9c98492bd9ffe7502a156a9a9cb
ronaldodeschain <lotushall@gmail.com>
2025-05-16 17:42:49 -0300

    Mais algumas pequenas alteraçoes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python_alura
 Repositório para os cursos da alura
+
 [Primeira aplicaçao](https://cursos.alura.com.br/course/python-crie-sua-primeira-aplicacao)
--- primeira_aplicacao/app.py
+++ primeira_aplicacao/app.py
@@ -1,1 +1,20 @@
-print('ola mundo!')
+# https://fsymbols.com/pt/ -> site para buscar simbolos a partir da escrita
+print("""
+      
+╱╱╱╱╱╱╭╮
+╱╱╱╱╱╱┃┃
+╭━━┳━━┫╰━┳━━┳━╮╭━━┳╮╭┳━━┳━┳━━┳━━┳━━╮
+┃━━┫╭╮┃╭╮┃╭╮┃╭╯┃┃━╋╋╋┫╭╮┃╭┫┃━┫━━┫━━┫
+┣━━┃╭╮┃╰╯┃╰╯┃┃╱┃┃━╋╋╋┫╰╯┃┃┃┃━╋━━┣━━┃
+╰━━┻╯╰┻━━┻━━┻╯╱╰━━┻╯╰┫╭━┻╯╰━━┻━━┻━━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
+      """)
+
+print('1. Cadastrar Restaurante')
+print('2. Listar Restaurantes')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opçao: ')
+print(f'voce escolheu a opçao {opcao_escolhida}')
