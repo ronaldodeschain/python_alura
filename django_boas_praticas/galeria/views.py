367f5d54e0d150b93fd12dae890c385bf06985a5
ronaldodeschain <lotushall@gmail.com>
2025-05-26 19:21:20 -0300

    atualizando

--- django_boas_praticas/galeria/views.py
+++ django_boas_praticas/galeria/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1> Alura Teste</h1>')
+    return HttpResponse('<h1> Alura Teste</h1><p>Mais um texto aqui para teste</p>')
+
+
 
