9b0cdf0666e51e3c43196f0633d81b74761962fa
ronaldodeschain <lotushall@gmail.com>
2025-05-27 11:50:49 -0300

    Criando o template do index

--- django_boas_praticas/galeria/views.py
+++ django_boas_praticas/galeria/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1> Alura Teste</h1><p>Mais um texto aqui para teste</p>')
+    return render(request,'galeria/index.html')
 
 
 
