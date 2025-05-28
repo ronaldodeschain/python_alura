c022f188000809979f6040f021428ba275877c67
ronaldodeschain <lotushall@gmail.com>
2025-05-28 20:33:51 -0300

    Avançando um pouco no código

--- django_persistencia/galeria/views.py
+++ django_persistencia/galeria/views.py
@@ -1,8 +1,13 @@
 
 from django.shortcuts import render
 
+dados = {
+    1: {"nome" : "Nebulosa de Andromeda!", "legenda": "poder furioso do Shun"},
+    2: {"nome" : "Galaxia distante","legenda": "tchan tchammmm tchan tchan tchan tchaannnn tchan!"}
+}
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html',{"cards":dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
