3b55e929fcf1208fdb7c63c82383ee9e4e8b8551
ronaldodeschain <lotushall@gmail.com>
2025-05-29 10:57:33 -0300

    update do arquivo

--- django_persistencia/galeria/views.py
+++ django_persistencia/galeria/views.py
@@ -1,12 +1,15 @@
 
 from django.shortcuts import render
 
-dados = {
-    1: {"nome" : "Nebulosa de Andromeda!", "legenda": "poder furioso do Shun"},
-    2: {"nome" : "Galaxia distante","legenda": "tchan tchammmm tchan tchan tchan tchaannnn tchan!"}
-}
+
 
 def index(request):
+
+    dados = {
+    1: {"nome" : "Nebulosa de Andromeda!", "legenda": "poder furioso do Shun"},
+    2: {"nome" : "Galaxia distante","legenda": "tchan tchammmm tchan tchan tchan tchaannnn tchan!"}
+    }
+    
     return render(request, 'galeria/index.html',{"cards":dados})
 
 def imagem(request):
