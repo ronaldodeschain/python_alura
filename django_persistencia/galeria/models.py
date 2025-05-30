58b695a5e6626fc570ab9ab7274e4a7e3ec112bd
ronaldodeschain <lotushall@gmail.com>
2025-05-30 11:42:09 -0300

    update antes do almoço

--- django_persistencia/galeria/models.py
+++ django_persistencia/galeria/models.py
@@ -8,5 +8,5 @@ class Fotografia(models.Model):
     descricao = models.TextField(null=False,blank=False)
     foto = models.CharField(max_length=100, null=False,blank=False)
 
-def __str__(self):
-    return f"Fotografia [nome={Self.nome}]"
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
