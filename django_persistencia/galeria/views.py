c14b4cbc81a3fdd63fe49723b5af317e2e70601b
ronaldodeschain <lotushall@gmail.com>
2025-05-30 17:19:33 -0300

    Fim do estudo de hj

--- django_persistencia/galeria/views.py
+++ django_persistencia/galeria/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from galeria.models import Fotografia
 
 
@@ -8,5 +8,6 @@ def index(request):
     fotografias = Fotografia.objects.all()
     return render(request, 'galeria/index.html',{"cards":fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request,foto_id):
+    fotografia = get_object_or_404(Fotografia,pk=foto_id)
+    return render(request, 'galeria/imagem.html',{"fotografia":fotografia})
