19c69d3d7ea02c337a6569b9b4ca7abadb7b4f9f
ronaldodeschain <lotushall@gmail.com>
2025-06-02 14:16:22 -0300

    Migrations adicionando campos adicionais no banco de dados

--- django_persistencia/galeria/models.py
+++ django_persistencia/galeria/models.py
@@ -3,10 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALAXIA","Galaxia"),
+        ("PLANETA","Planeta")
+    ]
+
     nome = models.CharField(max_length=100, null=False,blank=False)
     legenda = models.CharField(max_length=150, null=False,blank=False)
+    categoria = models.CharField(max_length=100,choices=OPCOES_CATEGORIA, default='',null=False,blank=False)
     descricao = models.TextField(null=False,blank=False)
     foto = models.CharField(max_length=100, null=False,blank=False)
+    publicada = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
