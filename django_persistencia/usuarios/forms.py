36223c3a12890a52028b663f69f0fb003dcc3734
ronaldodeschain <lotushall@gmail.com>
2025-06-05 20:36:55 -0300

    Criando forms para cadastro

---
+++ django_persistencia/usuarios/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(label="Nome do Login",required=True,
+    max_length=100
+    )
+    senha=forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput()
+    )
