6e94f070f18e1233f4e5b000187f03239dcd2cda
ronaldodeschain <lotushall@gmail.com>
2025-06-09 20:17:03 -0300

    Fim do intervalo

--- django_persistencia/galeria/views.py
+++ django_persistencia/galeria/views.py
@@ -1,7 +1,7 @@
 
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from galeria.models import Fotografia
-
+from django.contrib import messages
 
 
 def index(request):
@@ -12,10 +12,18 @@ def index(request):
     return render(request, 'galeria/index.html',{"cards":fotografias})
 
 def imagem(request,foto_id):
+    if not request.user.is_authenticated:
+        messages.error(request,f'Precisa estar logado!')
+        return redirect('login')
+    
     fotografia = get_object_or_404(Fotografia,pk=foto_id)
     return render(request, 'galeria/imagem.html',{"fotografia":fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request,f'Precisa estar logado!')
+        return redirect('login')
+    
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
 
     if "buscar" in request.GET:
