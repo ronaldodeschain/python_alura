aaf7868ff199fd31218e7353bda7b8df9d777bc1
ronaldodeschain <lotushall@gmail.com>
2025-06-09 17:10:19 -0300

    Commit antes de ir para o Senac

--- django_persistencia/usuarios/forms.py
+++ django_persistencia/usuarios/forms.py
@@ -2,11 +2,69 @@ from django import forms
 
 class LoginForms(forms.Form):
     nome_login = forms.CharField(label="Nome do Login",required=True,
-    max_length=100
+    max_length=100,
+    widget=forms.TextInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Ex: Erasmo Carlos"
+            }
+        )
     )
     senha=forms.CharField(
         label="Senha",
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Digite sua senha"
+            }
+        )
     )
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Ex:Erasmo Carlos"
+            }           
+        )
+    )
+    email=forms.EmailField(
+        label="Email de Cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Ex: erasmocarlos@xpto.com"
+            }
+        )
+    )
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Digite sua senha"
+            }
+        )
+    )
+    senha_2 = forms.CharField(
+        label="Confirme sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Digite sua senha novamente"
+            }
+        )
+    )
+
